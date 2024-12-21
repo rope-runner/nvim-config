fb3ab338a1887c0aa3a70bf61eac92f4a1d0c389
rope_runner <gorovoy.operating@gmail.com>
2024-12-21 16:30:54 +0200

    some new stuff

--- lua/rope_runner/init.lua
+++ lua/rope_runner/init.lua
@@ -1,3 +1,23 @@
 require("rope_runner.remap")
 require("rope_runner.packer")
 require("rope_runner.set")
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
